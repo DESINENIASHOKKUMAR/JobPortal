ddf3fd97e4f4ec9ab2286a6eeb8a27cb2793f4a3
DESINENIASHOKKUMAR <140548724+DESINENIASHOKKUMAR@users.noreply.github.com>
2024-08-05 23:39:34 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passportConfig = require("./lib/passportConfig");
+const cors = require("cors");
+const fs = require("fs");
+require('dotenv').config();
+// MongoDB
+const MONGO_URI = process.env.MONGO_URI;
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected successfully"))
+  .catch((e) => console.log(e));
+
+
+// initialising directories
+if (!fs.existsSync("./public")) {
+  fs.mkdirSync("./public");
+}
+if (!fs.existsSync("./public/resume")) {
+  fs.mkdirSync("./public/resume");
+}
+if (!fs.existsSync("./public/profile")) {
+  fs.mkdirSync("./public/profile");
+}
+
+const app = express();
+const port = 4444;
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// Setting up middlewares
+app.use(cors());
+app.use(express.json());
+app.use(passportConfig.initialize());
+
+// Routing
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/api", require("./routes/apiRoutes"));
+app.use("/upload", require("./routes/uploadRoutes"));
+app.use("/host", require("./routes/downloadRoutes"));
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}!`);
+});
+
